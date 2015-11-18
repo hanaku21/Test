6cbe54b935bd392e12de7b7e8a2ee404bf279c78
hanaku21 <fon_uta_21@hotmail.com>
2015-11-18 18:48:29 +0700

    Solution
    Tree homework Final

---
+++ Test/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.InputButton = new System.Windows.Forms.Button();
+            this.InputTexbox = new System.Windows.Forms.TextBox();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.SuspendLayout();
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.textBox2.Location = new System.Drawing.Point(291, 54);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(268, 318);
+            this.textBox2.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Tree";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(288, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Result Node";
+            // 
+            // InputButton
+            // 
+            this.InputButton.Location = new System.Drawing.Point(460, 4);
+            this.InputButton.Name = "InputButton";
+            this.InputButton.Size = new System.Drawing.Size(99, 30);
+            this.InputButton.TabIndex = 2;
+            this.InputButton.Text = "Input Data";
+            this.InputButton.UseVisualStyleBackColor = true;
+            this.InputButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // InputTexbox
+            // 
+            this.InputTexbox.Location = new System.Drawing.Point(15, 10);
+            this.InputTexbox.Name = "InputTexbox";
+            this.InputTexbox.Size = new System.Drawing.Size(439, 20);
+            this.InputTexbox.TabIndex = 1;
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(13, 55);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(263, 317);
+            this.treeView1.TabIndex = 5;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(571, 384);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.InputTexbox);
+            this.Controls.Add(this.InputButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Name = "Form1";
+            this.Text = "Binary Tree Calculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button InputButton;
+        private System.Windows.Forms.TextBox InputTexbox;
+        private System.Windows.Forms.TreeView treeView1;
+    }
+}
+
---
+++ Test/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //initialize nothing just display window
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            treeView1.Nodes.Clear();
+            x = InputTexbox.Text;
+            x = x.Trim();
+            CreateTree();
+        }
+
+        string x;
+        int tlength;
+        string equation = "+-*/";
+
+        void CreateTree()
+        {
+            tlength = x.Length - 1;
+            TreeNode mtree = LeafTree();
+            treeView1.Nodes.Add(mtree);
+        }
+
+        TreeNode LeafTree()
+        {
+            if( tlength > -1)
+            {
+                if (equation.Contains(x[tlength]))
+                {
+                    char m = x[tlength];
+                    tlength -= 1;
+                    TreeNode t1 = LeafTree();
+                    tlength -= 1;
+                    TreeNode t2 = LeafTree();
+                    TreeNode[] at = null;
+                    if (t1 == null)
+                        at = new TreeNode[] { t2 };
+                    else if (t2 == null)
+                        at = new TreeNode[] { t1 };
+                    else
+                        at = new TreeNode[] { t1, t2 };
+                    return new TreeNode(m.ToString(), at);
+                }
+                else return new TreeNode(x[tlength].ToString());
+            }
+            return null;
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            textBox2.Text = string.Empty;
+            string m = treeView1.SelectedNode.Text;
+            if (equation.Contains(m))
+            {
+
+                string n = treeView1.SelectedNode.Nodes[0].Text;
+                TreeNode i = treeView1.SelectedNode;
+                m = FindLeafNode(treeView1.SelectedNode);
+                if (m.Length < 6)
+                {
+                    m = m.Replace('(', ' ');
+                    m = m.Replace(')', ' ');
+                    m = m.Trim();
+                }
+                else
+                {
+                    m = m.Substring(1, m.Length - 2);
+                }
+                double Result = CalLeafNode(treeView1.SelectedNode);
+                m = m + " = " + Result.ToString();
+                textBox2.Text = m;
+            }
+            else textBox2.Text = m;
+        }
+
+        string FindLeafNode(TreeNode i)
+        {
+            int k;
+            string ist = i.Text;
+            k = i.GetNodeCount(false);
+            if (k > 0)
+                return "(" + FindLeafNode(i.Nodes[1]) + ist + FindLeafNode(i.Nodes[0]) + ")";
+            else return ist;
+        }
+
+        double CalLeafNode(TreeNode i)
+        {
+            int k;
+            string ist = i.Text;
+            k = i.GetNodeCount(false);
+    
+            if (k > 0 && equation.Contains(ist))
+                switch (Convert.ToChar(ist))
+                {
+                    case '+': return CalLeafNode(i.Nodes[1]) + CalLeafNode(i.Nodes[0]); 
+                    case '-': return CalLeafNode(i.Nodes[1]) - CalLeafNode(i.Nodes[0]);
+                    case '*': return CalLeafNode(i.Nodes[1]) * CalLeafNode(i.Nodes[0]);
+                    case '/': return CalLeafNode(i.Nodes[1]) / CalLeafNode(i.Nodes[0]);
+                    default: return 0;
+                }      
+            else return Convert.ToDouble(ist);
+        }
+    }
+}
